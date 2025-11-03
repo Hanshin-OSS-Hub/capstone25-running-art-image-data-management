f9109a48cbdc06f97411dc23866135fc462f71c0
Duskafka <rlarlgus0206@naver.com>
2025-11-03 12:52:16 +0900

    init

---
+++ src/main/java/com/aetheriadm/AetheriaDmApplication.java
@@ -0,0 +1,13 @@
+package com.aetheriadm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AetheriaDmApplication {
+    // 애플리케이션 추가
+    public static void main(String[] args) {
+        SpringApplication.run(AetheriaDmApplication.class, args);
+    }
+
+}
