2c05bac1e61c02f38ba09f33d9e8621bb3cbe529
Duskafka <rlarlgus0206@naver.com>
2025-11-03 19:56:06 +0900

    feat(#5): 사용자의 ID를 사용하여 이미지 메타데이터를 조회하는 메소드를 구현

--- src/main/java/com/aetheriadm/imagemetatdata/infrastructure/repository/ImageMetadataJpaRepository.java
+++ src/main/java/com/aetheriadm/imagemetatdata/infrastructure/repository/ImageMetadataJpaRepository.java
@@ -4,6 +4,9 @@ import com.aetheriadm.imagemetatdata.domain.ImageMetadata;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ImageMetadataJpaRepository extends JpaRepository<ImageMetadata, Long> {
+    List<ImageMetadata> findAllByRunnerId(Long runnerId);
 }
