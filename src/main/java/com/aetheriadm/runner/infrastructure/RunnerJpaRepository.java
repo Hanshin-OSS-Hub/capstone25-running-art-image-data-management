a54706f4d6865ddcc6c77f29a458dba3a13fa0eb
Duskafka <rlarlgus0206@naver.com>
2025-11-03 20:31:11 +0900

    feat(#5): 러너 기능 구현

---
+++ src/main/java/com/aetheriadm/runner/infrastructure/RunnerJpaRepository.java
@@ -0,0 +1,11 @@
+package com.aetheriadm.runner.infrastructure;
+
+import com.aetheriadm.runner.domain.Runner;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RunnerJpaRepository extends JpaRepository<Runner, Long> {
+    Boolean existByKakaoId(Long kakaoId);
+    void deleteByKakaoId(Long kakaoId);
+}
---
+++ src/main/java/com/aetheriadm/runner/infrastructure/RunnerQueryRepository.java
@@ -0,0 +1,24 @@
+package com.aetheriadm.runner.infrastructure;
+
+import com.aetheriadm.runner.domain.Runner;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/**
+ * 데이터베이스의 상태를 변경하지 않고, 데이터를 조회하는 작업을 하는 레포지토리
+ */
+@Repository
+@RequiredArgsConstructor
+public class RunnerQueryRepository {
+    private final RunnerJpaRepository runnerJpaRepository;
+
+    public Boolean existByKakaoId(Long kakaoId) {
+        return runnerJpaRepository.existByKakaoId(kakaoId);
+    }
+
+    public Optional<Runner> retrieveById(Long runnerId) {
+        return runnerJpaRepository.findById(runnerId);
+    }
+}
