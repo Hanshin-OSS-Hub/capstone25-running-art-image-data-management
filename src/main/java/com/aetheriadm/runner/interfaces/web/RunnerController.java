c0ee4766181da5a5712df13222e249886e4033cd
Duskafka <rlarlgus0206@naver.com>
2025-11-03 20:34:48 +0900

    feat(#5): 러너 컨트롤러 구현

---
+++ src/main/java/com/aetheriadm/runner/interfaces/web/RunnerController.java
@@ -0,0 +1,35 @@
+package com.aetheriadm.runner.interfaces.web;
+
+import com.aetheriadm.runner.application.RunnerService;
+import com.aetheriadm.runner.interfaces.dto.request.RunnerCreateRequest;
+import com.aetheriadm.runner.interfaces.dto.response.RunnerResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/runner")
+@RequiredArgsConstructor
+public class RunnerController {
+    private final RunnerService runnerService;
+
+    @PostMapping
+    public ResponseEntity<Long> createRunner(@RequestBody RunnerCreateRequest request) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(runnerService.createRunner(request));
+    }
+
+    @GetMapping
+    public ResponseEntity<RunnerResponse> retrieveRunner(Long runnerId) {
+        RunnerResponse runnerResponse = runnerService.retrieveRunner(runnerId);
+        return ResponseEntity.status(HttpStatus.OK).body(runnerResponse);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteRunner(Long runnerId) {
+        runnerService.deleteRunner(runnerId);
+        return ResponseEntity.noContent().build();
+    }
+}
